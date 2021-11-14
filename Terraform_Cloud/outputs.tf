4b9e0323b4f13b4a4e79cf600669b29b07e04329
trevor-anderson <T.AndersonProperty@gmail.com>
2021-11-14 10:53:37 -0500

    fix(workspaces-output): restructure output to keep non-sensitive values

--- Terraform_Cloud/outputs.tf
+++ Terraform_Cloud/outputs.tf
@@ -5,12 +5,16 @@ output "Organization" {
   value = data.tfe_organization.Nerdware
 }
 
-output "Workspaces" {
-  value = {
+locals {
+  workspaces_output = {
     for ws_name, ws_config in tfe_workspace.map : ws_name => {
       for key, value in ws_config : key => value if !can(nonsensitive(value)) && key != "vcs_repo"
     }
   }
+}
+
+output "Workspaces" {
+  value = local.workspaces_output
 }
 
 ###################################################
