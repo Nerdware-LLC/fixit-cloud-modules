a529a7dc5e9d0d1e59185421a158814b08a0beac
trevor-anderson <T.AndersonProperty@gmail.com>
2021-12-03 08:25:25 -0500

    fix(pw-policy): update params to meet CIS bm reqs

--- AWS_LandingZone/main.Account_Configs.tf
+++ AWS_LandingZone/main.Account_Configs.tf
@@ -2,12 +2,17 @@
 ### Account Password Policy
 
 resource "aws_iam_account_password_policy" "Account_PW_Policy" {
-  minimum_password_length        = 8 # TODO CIS recommends 14
-  require_lowercase_characters   = true
-  require_numbers                = true
-  require_uppercase_characters   = true
-  require_symbols                = true
+  # Password complexity settings:
+  minimum_password_length      = 14
+  require_lowercase_characters = true
+  require_uppercase_characters = true
+  require_numbers              = true
+  require_symbols              = true
+  # Settings that control HOW/WHEN passwords are reset:
+  max_password_age               = 90
+  password_reuse_prevention      = 5
   allow_users_to_change_password = true
+  hard_expiry                    = false # Admin-only reset after expiry.
 }
 
 #--------------------------------------------------
