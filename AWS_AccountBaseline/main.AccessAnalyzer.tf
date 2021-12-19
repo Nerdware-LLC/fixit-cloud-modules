2cbbbc5e0c85e24a2e7e21e637d66e8d2ca7d59a
trevor-anderson <T.AndersonProperty@gmail.com>
2021-12-19 12:34:14 -0500

    refactor: give access analyzer resource Org_ prefix to denote its purpose

--- AWS_AccountBaseline/main.AccessAnalyzer.tf
+++ AWS_AccountBaseline/main.AccessAnalyzer.tf
@@ -1,7 +1,7 @@
 ######################################################################
 ### Organization Access Analyzer
 
-resource "aws_accessanalyzer_analyzer" "this" {
+resource "aws_accessanalyzer_analyzer" "Org_AccessAnalyzer" {
   count = local.IS_ROOT_ACCOUNT ? 1 : 0
 
   analyzer_name = var.org_access_analyzer.name
