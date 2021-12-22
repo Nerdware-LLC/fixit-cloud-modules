dbfcb2f7f36fc42414f2f2bda1dad18f4b12f821
trevor-anderson <T.AndersonProperty@gmail.com>
2021-12-22 14:07:29 -0500

    feat: add regional replica keys

--- AWS_AccountBaseline/main.Org_KMS.tf
+++ AWS_AccountBaseline/main.Org_KMS.tf
@@ -1,28 +1,19 @@
 ######################################################################
 ### Organization Services KMS Key
 
-resource "aws_kms_key" "Org_KMS_Key" {
-  count = local.IS_ROOT_ACCOUNT ? 1 : 0
+locals {
+  # alias can be used cross-region in lieu of getting region-specific key ID
+  org_kms_key_alias = "alias/${var.org_kms_key.alias_name}"
 
-  description = coalesce(
+  key_description = coalesce(
     var.org_kms_key.description,
-    "A KMS key to encrypt Log-Archive files, as well as data streams for services like CloudTrail, CloudWatch, and SNS."
+    "A multi-region KMS key to encrypt Log-Archive files and data streams for Org-wide services like CloudTrail, CloudWatch, and SNS."
   )
-  policy              = one(data.aws_iam_policy_document.Org_KMS_Key_Policy).json
-  multi_region        = true
-  enable_key_rotation = true
-  tags                = var.org_kms_key.tags
 }
 
 #---------------------------------------------------------------------
 ### Org Services KMS Key ALIAS
 
-locals {
-  org_kms_key_alias = "alias/${var.org_kms_key.alias_name}"
-}
-
-/* The key's alias makes it easier for the Log-Archive account to use
-the key (necessary for CloudWatch Logs and S3 log bucket encryption) */
 resource "aws_kms_alias" "Org_KMS_Key" {
   count = local.IS_ROOT_ACCOUNT ? 1 : 0
 
@@ -31,160 +22,278 @@ resource "aws_kms_alias" "Org_KMS_Key" {
 }
 
 #---------------------------------------------------------------------
-### Org Services KMS Key POLICY
+### Org Services KMS Key - us-east-2 (ORIGINAL KEY)
 
-# NOTE: in KMS key policies, all 'resource' values must = "*" (points to the key itself)
-
-data "aws_iam_policy_document" "Org_KMS_Key_Policy" {
+resource "aws_kms_key" "Org_KMS_Key" {
   count = local.IS_ROOT_ACCOUNT ? 1 : 0
 
-  policy_id = var.org_kms_key.key_policy_id
-
-  statement {
-    sid    = "Enable IAM User Permissions"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = [local.root_account_id, var.log_archive_account_id]
-    }
-    actions   = ["kms:*"]
-    resources = ["*"]
-  }
-
-  statement {
-    sid    = "Allow CloudTrail to encrypt logs with this key"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions   = ["kms:GenerateDataKey*"]
-    resources = ["*"]
-    condition {
-      test     = "ArnLike"
-      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
-      values   = ["arn:aws:cloudtrail:*:${local.root_account_id}:trail/*"]
-    }
-    condition {
-      test     = "ArnEquals"
-      variable = "aws:SourceArn"
-      values   = ["arn:aws:cloudtrail:${local.aws_region}:${local.root_account_id}:trail/${var.org_cloudtrail.name}"]
-    }
-  }
-
-  statement {
-    sid    = "Allow CloudTrail to describe key"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["cloudtrail.amazonaws.com"]
-    }
-    actions   = ["kms:DescribeKey"]
-    resources = ["*"]
-  }
-
-  statement {
-    sid    = "Allow principals in the account to decrypt log files"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    actions   = ["kms:Decrypt", "kms:ReEncryptFrom"]
-    resources = ["*"]
-    condition {
-      test     = "StringEquals"
-      variable = "kms:CallerAccount"
-      values   = [local.root_account_id, var.log_archive_account_id]
-    }
-    condition {
-      test     = "StringLike"
-      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
-      values   = ["arn:aws:cloudtrail:*:${local.root_account_id}:trail/*"]
-    }
-  }
-
-  statement {
-    sid    = "Allow alias creation during setup"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    actions   = ["kms:CreateAlias"]
-    resources = ["*"]
-    condition {
-      test     = "StringEquals"
-      variable = "kms:ViaService"
-      values   = ["ec2.${local.aws_region}.amazonaws.com"]
-    }
-    condition {
-      test     = "StringEquals"
-      variable = "kms:CallerAccount"
-      values   = [local.root_account_id, var.log_archive_account_id]
-    }
-  }
-
-  statement {
-    sid    = "Enable cross account log decryption"
-    effect = "Allow"
-    principals {
-      type        = "AWS"
-      identifiers = ["*"]
-    }
-    actions   = ["kms:Decrypt", "kms:ReEncryptFrom"]
-    resources = ["*"]
-    condition {
-      test     = "StringEquals"
-      variable = "kms:CallerAccount"
-      values   = [local.root_account_id, var.log_archive_account_id]
-    }
-    condition {
-      test     = "StringLikeIfExists"
-      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
-      values   = ["arn:aws:cloudtrail:*:${local.root_account_id}:trail/*"]
-    }
-  }
-
-  # CloudWatch Log Group:
-  statement {
-    sid    = "CloudWatch_KMS_Policy"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["logs.${local.aws_region}.amazonaws.com"]
-    }
-    actions = [
-      "kms:Encrypt*",
-      "kms:Decrypt*",
-      "kms:ReEncrypt*",
-      "kms:GenerateDataKey*",
-      "kms:Describe*"
+  description         = local.key_description
+  multi_region        = true
+  enable_key_rotation = true
+  tags                = var.org_kms_key.tags
+  # NOTE: in KMS key policies, all 'resource' values must = "*" (points to the key itself)
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = var.org_kms_key.key_policy_id
+    Statement = [
+      # KEY POLICIES --> IAM Users/Roles
+      {
+        Sid       = "EnableAdminIAMUserPermissions"
+        Effect    = "Allow"
+        Principal = { AWS = [local.root_account_arn, local.log_archive_account_arn] }
+        Action    = "kms:*"
+        Resource  = "*"
+        StringEquals = {
+          "aws:PrincipalOrgID" = [local.org_id]
+        }
+      },
+      {
+        Sid       = "EnableCrossAccountLogDecryptionAndAliasCreation"
+        Effect    = "Allow"
+        Principal = { AWS = "*" }
+        Action    = ["kms:Decrypt", "kms:ReEncryptFrom", "kms:CreateAlias"]
+        Resource  = "*"
+        Condition = {
+          StringEquals = {
+            "kms:CallerAccount" = [local.root_account_arn, local.log_archive_account_arn]
+          }
+          StringEquals = {
+            "aws:PrincipalOrgID" = [local.org_id]
+          }
+        }
+      },
+      # KEY POLICIES --> CloudTrail
+      {
+        Sid       = "AllowCloudTrailEncryptLogsWithKey"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "kms:GenerateDataKey*"
+        Resource  = "*"
+        Condition = {
+          ArnLike = {
+            "kms:EncryptionContext:aws:cloudtrail:arn" = ["arn:aws:cloudtrail:*:${local.root_account_id}:trail/*"]
+          }
+          ArnLike = {
+            "aws:SourceArn" = ["arn:aws:cloudtrail:*:${local.root_account_id}:trail/${var.org_cloudtrail.name}"]
+          }
+        }
+      },
+      {
+        Sid       = "AllowCloudTrailDescribeKey"
+        Effect    = "Allow"
+        Principal = { Service = "cloudtrail.amazonaws.com" }
+        Action    = "kms:DescribeKey"
+        Resource  = "*"
+      },
+      # KEY POLICIES --> AWS-Config
+      {
+        Sid       = "AWSConfigKMSPolicy"
+        Effect    = "Allow"
+        Principal = { AWS = local.config_iam_role_arn } # NOT the service, since we have Config assume an IAM role.
+        Action    = ["kms:Decrypt", "kms:GenerateDataKey*"]
+        Resource  = "*"
+        Condition = {
+          StringEquals = {
+            "aws:PrincipalOrgID" = [local.org_id]
+          }
+        }
+      }
     ]
-    resources = ["*"]
-    condition {
-      test     = "ArnEquals"
-      variable = "kms:EncryptionContext:aws:logs:arn"
-      values   = ["arn:aws:logs:${local.aws_region}:${local.root_account_id}:log-group:${local.cw_log_grp.name}"]
-    }
-  }
+  })
+}
 
-  # AWS-Config:
-  statement {
-    sid    = "AWS-Config_KMS_Policy"
-    effect = "Allow"
-    principals {
-      type        = "Service"
-      identifiers = ["config.amazonaws.com"]
-    }
-    actions   = ["kms:Decrypt", "kms:GenerateDataKey*"]
-    resources = ["*"]
-    condition {
-      test     = "StringEquals"
-      variable = "aws:PrincipalOrgID"
-      values   = [local.org_id]
-    }
-  }
+######################################################################
+### REGIONAL REPLICA KEYS:
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - ap-northeast-1
+
+resource "aws_kms_replica_key" "ap-northeast-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.ap-northeast-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - ap-northeast-2
+
+resource "aws_kms_replica_key" "ap-northeast-2" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.ap-northeast-2
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - ap-northeast-3
+
+resource "aws_kms_replica_key" "ap-northeast-3" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.ap-northeast-3
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - ap-south-1
+
+resource "aws_kms_replica_key" "ap-south-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.ap-south-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - ap-southeast-1
+
+resource "aws_kms_replica_key" "ap-southeast-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.ap-southeast-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - ap-southeast-2
+
+resource "aws_kms_replica_key" "ap-southeast-2" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.ap-southeast-2
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - ca-central-1
+
+resource "aws_kms_replica_key" "ca-central-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.ca-central-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - eu-north-1
+
+resource "aws_kms_replica_key" "eu-north-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.eu-north-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - eu-central-1
+
+resource "aws_kms_replica_key" "eu-central-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.eu-central-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - eu-west-1
+
+resource "aws_kms_replica_key" "eu-west-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.eu-west-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - eu-west-2
+
+resource "aws_kms_replica_key" "eu-west-2" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.eu-west-2
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - eu-west-3
+
+resource "aws_kms_replica_key" "eu-west-3" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.eu-west-3
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - sa-east-1
+
+resource "aws_kms_replica_key" "sa-east-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.sa-east-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - us-east-1
+
+resource "aws_kms_replica_key" "us-east-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.us-east-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - us-west-1
+
+resource "aws_kms_replica_key" "us-west-1" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.us-west-1
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
+}
+
+#---------------------------------------------------------------------
+# Org_KMS_Key Replica - us-west-2
+
+resource "aws_kms_replica_key" "us-west-2" {
+  count    = local.IS_ROOT_ACCOUNT ? 1 : 0
+  provider = aws.us-west-2
+
+  primary_key_arn         = one(aws_kms_key.Org_KMS_Key).arn
+  description             = local.key_description
+  deletion_window_in_days = 7
 }
 
 ######################################################################
