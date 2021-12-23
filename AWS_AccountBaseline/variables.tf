00fc5db597d999f4026f4f0ff43570430099e811
trevor-anderson <T.AndersonProperty@gmail.com>
2021-12-23 07:40:43 -0500

    style: change order of variable decl's to match file name order

--- AWS_AccountBaseline/variables.tf
+++ AWS_AccountBaseline/variables.tf
@@ -27,37 +27,6 @@ variable "security_account_id" {
 }
 
 #---------------------------------------------------------------------
-### Log Archive Variables:
-
-variable "org_log_archive_s3_bucket" {
-  description = "Configs for the S3 bucket used as the Organization's log archive."
-  type = object({
-    name = string
-    tags = optional(map(string))
-    access_logs_s3 = object({
-      name = string
-      tags = optional(map(string))
-    })
-  })
-}
-
-#---------------------------------------------------------------------
-### Organization Services KMS Key Variables:
-
-variable "org_kms_key" {
-  description = <<-EOF
-  Config object for the KMS key used to encrypt Log-Archive files, as well as
-  data streams from CloudTrail, CloudWatch, SNS, etc.
-  EOF
-  type = object({
-    alias_name    = string
-    description   = optional(string)
-    key_policy_id = optional(string)
-    tags          = optional(map(string))
-  })
-}
-
-#---------------------------------------------------------------------
 ### Access Analyzer Variables:
 
 variable "org_access_analyzer" {
@@ -65,58 +34,6 @@ variable "org_access_analyzer" {
   type = object({
     name = string
     tags = optional(map(string))
-  })
-}
-
-#---------------------------------------------------------------------
-### Default VPC Component Variables:
-
-variable "default_vpc_component_tags" {
-  description = <<-EOF
-  In accordance with best practices, this module locks down the default VPC and all
-  its components to ensure all ingress/egress traffic only uses infrastructure with
-  purposefully-designed rules and configs. Default subnets must be deleted manually
-  - they cannot be removed via Terraform. This variable allows you to customize the
-  tags on these "default" network components; defaults will be used if not provided.
-  EOF
-  type = object({
-    default_vpc            = optional(map(string))
-    default_route_table    = optional(map(string))
-    default_network_acl    = optional(map(string))
-    default_security_group = optional(map(string))
-  })
-  default = {
-    default_vpc            = null
-    default_route_table    = null
-    default_network_acl    = null
-    default_security_group = null
-  }
-}
-
-#---------------------------------------------------------------------
-### CloudTrail Variables:
-
-variable "org_cloudtrail" {
-  description = <<-EOF
-  Config object for the Organization CloudTrail. The "cloudwatch_config"
-  object is used to configure the CloudWatch Logs log group and its
-  associated IAM service role which receives logs from the Org's CloudTrail.
-  EOF
-  type = object({
-    name = string
-    tags = optional(map(string))
-    cloudwatch_config = object({
-      log_group = object({
-        name              = string
-        retention_in_days = optional(number)
-        tags              = optional(map(string))
-      })
-      iam_service_role = object({
-        name        = string
-        policy_name = optional(string)
-        tags        = optional(map(string))
-      })
-    })
   })
 }
 
@@ -157,6 +74,46 @@ variable "org_aws_config" {
 }
 
 #---------------------------------------------------------------------
+### CloudWatch Alarm Variables:
+
+variable "cloudwatch_alarms" {
+  description = "Config object for CIS-Benchmark CloudWatch Alarms."
+  type = object({
+    namespace = string
+    sns_topic = object({
+      name         = string
+      display_name = optional(string)
+      tags         = optional(map(string))
+    })
+  })
+}
+
+#---------------------------------------------------------------------
+### Default VPC Component Variables:
+
+variable "default_vpc_component_tags" {
+  description = <<-EOF
+  In accordance with best practices, this module locks down the default VPC and all
+  its components to ensure all ingress/egress traffic only uses infrastructure with
+  purposefully-designed rules and configs. Default subnets must be deleted manually
+  - they cannot be removed via Terraform. This variable allows you to customize the
+  tags on these "default" network components; defaults will be used if not provided.
+  EOF
+  type = object({
+    default_vpc            = optional(map(string))
+    default_route_table    = optional(map(string))
+    default_network_acl    = optional(map(string))
+    default_security_group = optional(map(string))
+  })
+  default = {
+    default_vpc            = null
+    default_route_table    = null
+    default_network_acl    = null
+    default_security_group = null
+  }
+}
+
+#---------------------------------------------------------------------
 ### GuardDuty Variables:
 
 variable "guard_duty_detector" {
@@ -176,17 +133,67 @@ variable "guard_duty_detector" {
 }
 
 #---------------------------------------------------------------------
-### CloudWatch Alarm Variables:
+### Log Archive Variables:
 
-variable "cloudwatch_alarms" {
-  description = "Config object for CIS-Benchmark CloudWatch Alarms."
+variable "org_log_archive_s3_bucket" {
+  description = <<-EOF
+  Config object for the Log-Archive account's titular S3 bucket used as
+  the Organization's log archive.
+  EOF
   type = object({
-    namespace = string
-    sns_topic = object({
-      name         = string
-      display_name = optional(string)
-      tags         = optional(map(string))
+    name = string
+    tags = optional(map(string))
+    access_logs_s3 = object({
+      name = string
+      tags = optional(map(string))
     })
+  })
+}
+
+#---------------------------------------------------------------------
+### CloudTrail Variables:
+
+variable "org_cloudtrail" {
+  description = "Config object for the Organization CloudTrail in the root account."
+  type = object({
+    name = string
+    tags = optional(map(string))
+  })
+}
+
+variable "org_cloudtrail_cloudwatch_logs_group" {
+  description = <<-EOF
+  Config object for the CloudWatch Logs log group and associated
+  IAM service role used by the Log-Archive account to receive
+  logs from the Organization's CloudTrail.
+  EOF
+  type = object({
+    log_group = object({
+      name              = string
+      retention_in_days = optional(number)
+      tags              = optional(map(string))
+    })
+    iam_service_role = object({
+      name        = string
+      policy_name = optional(string)
+      tags        = optional(map(string))
+    })
+  })
+}
+
+#---------------------------------------------------------------------
+### Organization Services KMS Key Variables:
+
+variable "org_kms_key" {
+  description = <<-EOF
+  Config object for the KMS key used to encrypt Log-Archive files, as well as
+  data streams from CloudTrail, CloudWatch, SNS, etc.
+  EOF
+  type = object({
+    alias_name    = string
+    description   = optional(string)
+    key_policy_id = optional(string)
+    tags          = optional(map(string))
   })
 }
 
