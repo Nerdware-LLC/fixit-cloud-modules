7e32820da7f9070ad26adbe0e19423645103206a
trevor-anderson <T.AndersonProperty@gmail.com>
2021-12-31 10:45:19 -0500

    fix(OrgAccAdminRole): update assume role policy to address auth failure issue

--- AWS_AccountBaseline/main.OrgAccountAccessRole.tf
+++ AWS_AccountBaseline/main.OrgAccountAccessRole.tf
@@ -12,18 +12,28 @@ resource "aws_iam_role" "OrganizationAccountAccessRole" {
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [
-      for admin_user, admin_ips in var.administrators : {
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${local.root_account_id}:user/${admin_user}"
+    Statement = flatten([
+      for admin_user, admin_ips in var.administrators : [
+        {
+          Effect = "Allow"
+          Principal = {
+            AWS = "arn:aws:iam::${local.root_account_id}:user/${admin_user}"
+          }
+          Action = "sts:AssumeRole"
+        },
+        {
+          Effect = "Deny"
+          Principal = {
+            AWS = "arn:aws:iam::${local.root_account_id}:user/${admin_user}"
+          }
+          Action = "sts:AssumeRole"
+          Condition = {
+            NotIpAddress = { "aws:SourceIp" = admin_ips }
+            Bool         = { "aws:ViaAWSService" = "false" }
+          }
         }
-        Action = "sts:AssumeRole"
-        Condition = {
-          IpAddress = { "aws:SourceIp" = admin_ips }
-        }
-      }
-    ]
+      ]
+    ])
   })
 }
 
