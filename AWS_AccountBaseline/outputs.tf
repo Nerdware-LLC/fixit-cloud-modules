43eb4c0bc5b68483dd1d3b2386e11a4f4af6a0ea
trevor-anderson <T.AndersonProperty@gmail.com>
2022-01-02 10:13:06 -0500

    fix(AccBase): add filter to Config Recorder outputs to kill erroneous diff msg

--- AWS_AccountBaseline/outputs.tf
+++ AWS_AccountBaseline/outputs.tf
@@ -55,23 +55,31 @@ output "Org_Config_Role_Policy" {
 output "Config_Recorders" {
   description = "A map of AWS-Config Recorders by region."
   value = {
-    "ap-northeast-1" = aws_config_configuration_recorder.ap-northeast-1
-    "ap-northeast-2" = aws_config_configuration_recorder.ap-northeast-2
-    "ap-northeast-3" = aws_config_configuration_recorder.ap-northeast-3
-    "ap-south-1"     = aws_config_configuration_recorder.ap-south-1
-    "ap-southeast-1" = aws_config_configuration_recorder.ap-southeast-1
-    "ap-southeast-2" = aws_config_configuration_recorder.ap-southeast-2
-    "ca-central-1"   = aws_config_configuration_recorder.ca-central-1
-    "eu-north-1"     = aws_config_configuration_recorder.eu-north-1
-    "eu-central-1"   = aws_config_configuration_recorder.eu-central-1
-    "eu-west-1"      = aws_config_configuration_recorder.eu-west-1
-    "eu-west-2"      = aws_config_configuration_recorder.eu-west-2
-    "eu-west-3"      = aws_config_configuration_recorder.eu-west-3
-    "sa-east-1"      = aws_config_configuration_recorder.sa-east-1
-    "us-east-1"      = aws_config_configuration_recorder.us-east-1
-    "us-east-2"      = aws_config_configuration_recorder.us-east-2
-    "us-west-1"      = aws_config_configuration_recorder.us-west-1
-    "us-west-2"      = aws_config_configuration_recorder.us-west-2
+    for region, recorder in {
+      "ap-northeast-1" = aws_config_configuration_recorder.ap-northeast-1
+      "ap-northeast-2" = aws_config_configuration_recorder.ap-northeast-2
+      "ap-northeast-3" = aws_config_configuration_recorder.ap-northeast-3
+      "ap-south-1"     = aws_config_configuration_recorder.ap-south-1
+      "ap-southeast-1" = aws_config_configuration_recorder.ap-southeast-1
+      "ap-southeast-2" = aws_config_configuration_recorder.ap-southeast-2
+      "ca-central-1"   = aws_config_configuration_recorder.ca-central-1
+      "eu-north-1"     = aws_config_configuration_recorder.eu-north-1
+      "eu-central-1"   = aws_config_configuration_recorder.eu-central-1
+      "eu-west-1"      = aws_config_configuration_recorder.eu-west-1
+      "eu-west-2"      = aws_config_configuration_recorder.eu-west-2
+      "eu-west-3"      = aws_config_configuration_recorder.eu-west-3
+      "sa-east-1"      = aws_config_configuration_recorder.sa-east-1
+      "us-east-1"      = aws_config_configuration_recorder.us-east-1
+      "us-east-2"      = aws_config_configuration_recorder.us-east-2
+      "us-west-1"      = aws_config_configuration_recorder.us-west-1
+      "us-west-2"      = aws_config_configuration_recorder.us-west-2
+      } : region => merge(recorder, {
+        # The purpose of this block is to kill the erroneous diff msg re: resource_types
+        recording_group = [for grp_config in recorder.recording_group : {
+          all_supported                 = grp_config.all_supported
+          include_global_resource_types = grp_config.include_global_resource_types
+        }]
+    })
   }
 }
 
