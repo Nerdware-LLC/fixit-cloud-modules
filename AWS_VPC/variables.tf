2252493b72627cac6493c8a5a489b3c488596f5d
trevor-anderson <T.AndersonProperty@gmail.com>
2022-01-13 11:52:03 -0500

    refactor(VPC-vars): bring back var security_groups as list of objs

--- AWS_VPC/variables.tf
+++ AWS_VPC/variables.tf
@@ -124,4 +124,125 @@ variable "default_resource_tags" {
   }
 }
 
+#---------------------------------------------------------------------
+### Network ACL Variables
+
+variable "network_acl_configs" {
+  description = <<-EOF
+  List of config objects for Network ACL resources. For ingress/egress rule config objects,
+  instead of providing separate "from_port" and "to_port" args, you can alternatively provide
+  just "port" which will map to both. You can also pass a string value like "HTTPS" or "Redis"
+  (case-insensitive) to the "default_port" arg to have the default port of the protocol/service
+  mapped to both. Refer to the README for a list of supported arguments for "default_port". If
+  you need the cidr_block of a particular AWS service, like EC2 Instance Connect, you can pass
+  cidr_block = "EC2_INSTANCE_CONNECT". Please refer to the README to for a list of supported
+  AWS services for "cidr_block".
+  EOF
+  type = list(object({
+    subnet_cidrs = list(string)
+    access = optional(object({
+      ingress = optional(list(object({
+        rule_number  = number
+        cidr_block   = string
+        protocol     = optional(string)
+        default_port = optional(string)
+        port         = optional(number)
+        from_port    = optional(number)
+        to_port      = optional(number)
+      })))
+      egress = optional(list(object({
+        rule_number  = number
+        cidr_block   = string
+        protocol     = optional(string)
+        default_port = optional(string)
+        port         = optional(number)
+        from_port    = optional(number)
+        to_port      = optional(number)
+      })))
+    }))
+    tags = optional(map(string))
+  }))
+  validation {
+    condition = alltrue([
+      for rule in flatten([var.network_acl_configs[*].access.ingress, var.network_acl_configs[*].access.egress]) : anytrue([
+        # Ensure all rules contain one of the following: default_port, OR port, OR (from_port AND to_port)
+        can(rule.from_port) && can(rule.to_port),
+        can(rule.port),
+        can(rule.default_port)
+      ])
+    ])
+    error_message = "Invalid port-defining properties on one or more network ACL rule config objects."
+  }
+}
+
+#---------------------------------------------------------------------
+### Security Group Variables
+
+variable "security_groups" {
+  description = <<-EOF
+  Map of Security Group name keys pointing to config object values. For ingress/egress rule
+  config objects, instead of providing separate "from_port" and "to_port" arguments, you can
+  alternatively just provide "port" which will map to both. For SecGrp ingress/egress rules
+  which need to point to another Security Group, use the "peer_security_group" key with the
+  value set to the name of the target SecGrp. If you need the cidr_blocks of a particular AWS
+  service, like EC2 Instance Connect, you can set "aws_service" = "EC2_INSTANCE_CONNECT".
+  Please refer to the README for a list of supported arguments for "aws_service".
+  EOF
+  type = list(object({
+    name        = string
+    description = string
+    tags        = optional(map(string))
+    access = object({
+      ingress = optional(list(object({
+        description            = string
+        protocol               = optional(string)
+        port                   = optional(number)
+        from_port              = optional(number)
+        to_port                = optional(number)
+        peer_security_group_id = optional(string)
+        peer_security_group    = optional(string)
+        aws_service            = optional(string)
+        cidr_blocks            = optional(list(string))
+        self                   = optional(bool)
+      })))
+      egress = optional(list(object({
+        description            = string
+        protocol               = optional(string)
+        port                   = optional(number)
+        from_port              = optional(number)
+        to_port                = optional(number)
+        peer_security_group_id = optional(string)
+        peer_security_group    = optional(string)
+        aws_service            = optional(string)
+        cidr_blocks            = optional(list(string))
+        self                   = optional(bool)
+      })))
+    })
+  }))
+  validation {
+    condition = alltrue(flatten([
+      for sec_grp_name, sec_grp_config in var.security_groups : [
+        for access_type, rule_list in sec_grp_config.access : [
+          for rule in rule_list : alltrue([
+            # Ensure all rules contain "port" OR ("from_port" AND "to_port")
+            anytrue([
+              can(rule.from_port) && can(rule.to_port),
+              can(rule.port)
+            ]),
+            # Ensure all rules contain exactly one of the following: peer_security_group_id OR peer_security_group OR aws_service OR cidr_blocks OR self
+            1 == length([
+              for rule_key in ["peer_security_group_id", "peer_security_group", "aws_service", "cidr_blocks", "self"] : rule_key
+              if alltrue([
+                can(rule[rule_key]),                                                          # check rule param existence
+                rule_key == "peer_security_group" && can(var.security_groups[rule[rule_key]]) # for peer_secgrp, ensure named SecGrp exists
+              ])
+            ])
+          ])
+        ]
+      ]
+    ]))
+    error_message = "Invalid combination of properties on one or more security group rule config objects."
+  }
+}
+
 ######################################################################
