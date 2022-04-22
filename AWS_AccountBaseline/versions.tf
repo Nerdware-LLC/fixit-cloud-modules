4903a89510734dd05aeeaab449432b7a1dd07eb1
trevor-anderson <T.AndersonProperty@gmail.com>
2022-04-22 13:13:51 -0400

    chore: update AWS provider to v4.11

--- AWS_AccountBaseline/versions.tf
+++ AWS_AccountBaseline/versions.tf
@@ -8,7 +8,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.74.0"
+      version = "~> 4.11.0"
 
       /* For services that require resources to be created in multiple
       regions (AWS-Config, GuardDuty, and SecurityHub), provider aliases
