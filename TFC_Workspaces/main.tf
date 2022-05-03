35857cdccdd7975107421005919503e0b619e36a
trevor-anderson <T.AndersonProperty@gmail.com>
2022-05-03 11:16:57 -0400

    refactor: add 'execution_mode' config to workspace resource, change var structure

--- TFC_Workspaces/main.tf
+++ TFC_Workspaces/main.tf
@@ -5,58 +5,49 @@ data "tfe_organization" "Nerdware" {
   name = "Nerdware"
 }
 
+#---------------------------------------------------------------------
+
 resource "tfe_workspace" "map" {
   for_each = var.workspaces
 
-  organization        = data.tfe_organization.Nerdware.name
-  name                = each.key
-  description         = each.value.description
+  organization   = data.tfe_organization.Nerdware.name
+  name           = each.key
+  description    = each.value.description
+  execution_mode = each.value.remote_execution != null ? "remote" : "local"
+
   working_directory   = each.value.modules_repo_dir
   allow_destroy_plan  = coalesce(each.value.is_destroyable, false)
-  speculative_enabled = coalesce(each.value.is_destroyable, false)
+  speculative_enabled = coalesce(each.value.is_speculative_plan_enabled, false)
   terraform_version   = "1.1.5"
   queue_all_runs      = false
 
   dynamic "vcs_repo" {
     for_each = (
-      each.value.is_vcs_connected == true
+      try(each.value.remote_execution.is_vcs_connected, false)
       ? ["Nerdware-LLC/fixit-cloud-modules"]
       : []
     )
     content {
-      identifier = vcs_repo.value
-      # The below variable is stored/provided by TF Cloud - do not fiddle with it.
-      oauth_token_id     = var.fixit-cloud-modules-repo_github-oauth-token-id
+      identifier         = vcs_repo.value
+      branch             = "main"
       ingress_submodules = false
+      # The below variable is stored/provided by TF Cloud - do not fiddle with it.
+      oauth_token_id = var.fixit-cloud-modules-repo_github-oauth-token-id
     }
   }
 }
 
 #---------------------------------------------------------------------
 
-locals {
-  /* This local provides a flat list of all workspace variable configs, along
-  with each variables respective "workspace_id" value. Note that variables
-  provided via var.variables_for_all_workspaces are included as well */
-  all_vars_with_workspace_ids = flatten([
-    for ws_name, ws_config in var.workspaces : [
-      (ws_config.variables == null ? [] : [
-        for ws_var_obj in ws_config.variables : merge(ws_var_obj, {
-          workspace_id = tfe_workspace.map[ws_name].id
-        })
-      ]),
-      [ # Here we add the variables in var.variables_for_all_workspaces
-        for all_ws_var_obj in var.variables_for_all_workspaces : merge(all_ws_var_obj, {
-          workspace_id = tfe_workspace.map[ws_name].id
-        })
-      ]
-    ]
-  ])
-}
-
 resource "tfe_variable" "map" {
   for_each = {
-    for ws_var in local.all_vars_with_workspace_ids : ws_var.key => ws_var
+    # Merge workspace_id into each workspace variable
+    for var_obj in flatten([
+      for workspace_name, workspace_config in var.workspaces : [
+        for var_obj in try(workspace_config.remote_execution.variables, []) :
+        merge(var_obj, { workspace_id = tfe_workspace.map[workspace_name].id })
+      ]
+    ]) : var_obj.key => var_obj # <-- each.key, each.value
   }
 
   workspace_id = each.value.workspace_id
--- TFC_Workspaces/variables.tf
+++ TFC_Workspaces/variables.tf
@@ -3,49 +3,37 @@
 
 variable "workspaces" {
   description = <<-EOF
-  Map of TF Cloud Workspace config objects. The optional boolean properties
-  all default to false. "is_vcs_connected", if "true", will connect the
+  Map of TF Cloud Workspace names to corresponding workspace config objects.
+  The optional boolean properties all default to false. Use the "remote_execution"
+  property to configure workspace plan/apply operations run remotely on TFC VMs
+  instead of your local machine. "is_vcs_connected", if true, will connect the
   workspace to the fixit-cloud-modules repo. Note that enabling the VCS-driven
-  workflow will DISABLE the ability to trigger runs via the CLI/API, which at
-  this time can only be reversed by manually removing the repository from the
-  workspace via the TF Cloud console. The "modules_repo_dir" param should be
-  the name of a dir in the Nerdware fixit-cloud-modules repository (e.g.,
-  "Terraform_Cloud").
+  workflow will DISABLE the ability to trigger runs via the CLI/API, which at this
+  time can only be reversed by manually removing the repository from the workspace
+  via the TF Cloud console. The "modules_repo_dir" param should be the name of a
+  dir in the Nerdware fixit-cloud-modules repository (e.g., "TFC_Workspaces").
   EOF
+
   type = map(object({
     description                 = optional(string)
-    modules_repo_dir            = optional(string)
-    is_vcs_connected            = optional(bool)
     is_destroyable              = optional(bool)
     is_speculative_plan_enabled = optional(bool)
-    variables = optional(list(object({
-      key          = string
-      value        = string
-      description  = optional(string)
-      is_env_var   = optional(bool)
-      is_value_hcl = optional(bool)
-      is_sensitive = optional(bool)
-    })))
+    modules_repo_dir            = optional(string)
+    remote_execution = optional(object({
+      is_vcs_connected = optional(bool)
+      variables = optional(list(object({
+        key          = string
+        value        = string
+        description  = optional(string)
+        is_env_var   = optional(bool)
+        is_value_hcl = optional(bool)
+        is_sensitive = optional(bool)
+      })))
+    }))
   }))
 }
 
 #---------------------------------------------------------------------
-
-variable "variables_for_all_workspaces" {
-  description = <<-EOF
-  Map of variable-names to objects which configure them. The vars included
-  here will be available to ALL workspaces within the organization.
-  EOF
-  type = list(object({
-    key          = string
-    value        = string
-    description  = optional(string)
-    is_env_var   = optional(bool)
-    is_value_hcl = optional(bool)
-    is_sensitive = optional(bool)
-  }))
-  default = []
-}
 
 variable "fixit-cloud-modules-repo_github-oauth-token-id" {
   description = <<-EOF
@@ -53,8 +41,9 @@ variable "fixit-cloud-modules-repo_github-oauth-token-id" {
   should *NOT* be provided as an input variable. This declaration
   exists solely to silence CLI warnings/errors.
   EOF
-  type        = string
-  sensitive   = true
+
+  type      = string
+  sensitive = true
 }
 
 ######################################################################
