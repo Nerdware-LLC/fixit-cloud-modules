15f90ead03b038f7fa4fa710f1442058724986b1
trevor-anderson <T.AndersonProperty@gmail.com>
2022-07-02 06:59:52 -0400

    chore: update pinned tf version

--- AWS_AccountBaseline/versions.tf
+++ AWS_AccountBaseline/versions.tf
@@ -2,7 +2,7 @@
 ### Terraform Version Info
 
 terraform {
-  required_version = "1.2.2"
+  required_version = "1.2.4"
   experiments      = [module_variable_optional_attrs]
 
   required_providers {
