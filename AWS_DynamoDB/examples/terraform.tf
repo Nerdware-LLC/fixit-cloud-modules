c5e6c955c201d750a5fa404c4f096b3c88d5f6d0
trevor-anderson <T.AndersonProperty@gmail.com>
2022-08-05 11:56:00 -0400

    feat(DynamoDB): add DynamoDB completed resources,outputs,examples

--- AWS_DynamoDB/examples/terraform.tf
+++ AWS_DynamoDB/examples/terraform.tf
@@ -1,7 +1,109 @@
 ######################################################################
 ### EXAMPLE USAGE: AWS_DynamoDB
 
+module "Foo_DynamoDB_SSE_KMS_Keys" {
+  /* Foo_DynamoDB_SSE_KMS_Keys module inputs */
+}
 
+module "Foo_Kinesis_Stream" {
+  /* Foo_Kinesis_Stream module inputs */
+}
 
+module "AWS_DynamoDB" {
+  source = "git@github.com:Nerdware-LLC/fixit-cloud-modules.git//AWS_DynamoDB"
+
+  table_name = "My_DynamoDB_Table"
+  attributes = [
+    {
+      name = "ID"
+      type = "N"
+    },
+    {
+      name = "Name"
+      type = "S"
+    },
+    {
+      name = "HighScore"
+      type = "N"
+    }
+  ]
+
+  partition_key = "ID"
+  sort_key      = "Name"
+
+  global_secondary_indexes = {
+    HighScoreGlobalIndex = {
+      partition_key      = "Name"
+      sort_key           = "HighScore"
+      projection_type    = "INCLUDE"
+      non_key_attributes = ["ID"]
+      capacity = {
+        read = {
+          max    = 10
+          target = 7
+          min    = 5
+        }
+        write = {
+          max    = 10
+          target = 8
+          min    = 5
+        }
+      }
+    }
+  }
+
+  server_side_encryption_kms_key_arn = (
+    # In this example, "us-west-1" is the region with the base table
+    module.Foo_DynamoDB_SSE_KMS_Keys.keys_by_table_region["us-west-1"].arn
+  )
+
+  billing_mode = "PROVISIONED" # <-- default
+  capacity = {
+    read = {
+      max    = 20
+      target = 13
+      min    = 10
+    }
+    write = {
+      max    = 20
+      target = 15
+      min    = 10
+    }
+  }
+
+  ttl = {
+    enabled        = true # <-- default
+    attribute_name = "TimeToExist"
+  }
+
+  streams = {
+    # You can have both types of streams if you want
+    dynamodb_stream_view_type = "NEW_AND_OLD_IMAGES"
+    kinesis_stream_arn        = module.Foo_Kinesis_Stream.Stream.arn
+  }
+
+  replicas = {
+    /* In the example below, each replica is provided with its own key.
+    However, depending on your use case, you may be able to simplify
+    resource/permissions management AND reduce KMS expenditure by instead
+    using a multi-region key. If you go that route, using an alias *could*
+    further reduce management overhead, but as with multi-region keys in
+    general, there are some important considerations of which you need to
+    be aware. Please refer to AWS KMS docs for more information.       */
+    for table_replica_region in ["us-east-1", "us-east-2"] : table_replica_region => {
+      propagate_tags                = true
+      enable_point_in_time_recovery = true # <-- default
+      kms_key_arn = lookup(
+        module.Foo_DynamoDB_SSE_KMS_Keys.keys_by_table_region,
+        table_replica_region # <-- "us-east-1", "us-east-2"
+      ).arn
+    }
+  }
+
+  tags = {
+    Name        = "My_DynamoDB_Table"
+    Environment = "production"
+  }
+}
 
 ######################################################################
---
+++ AWS_DynamoDB/main.DynamoDB_Table.tf
@@ -0,0 +1,126 @@
+######################################################################
+### AWS DynamoDB
+
+# The ignored tfsec rule below pertains to DAX clusters, which this module does not yet support.
+# tfsec:ignore:aws-dynamodb-enable-at-rest-encryption
+resource "aws_dynamodb_table" "this" {
+  name      = var.table_name
+  hash_key  = var.partition_key
+  range_key = var.sort_key
+
+  billing_mode   = var.billing_mode
+  read_capacity  = try(var.capacity.read.max, null)
+  write_capacity = try(var.capacity.write.max, null)
+  table_class    = var.table_storage_class
+
+  # ATTRIBUTES
+  dynamic "attribute" {
+    for_each = { for attr in var.attributes : attr.name => attr.type }
+
+    content {
+      name = attribute.key
+      type = attribute.value
+    }
+  }
+
+  # GLOBAL SECONDARY INDEXES
+  dynamic "global_secondary_index" {
+    for_each = var.global_secondary_indexes != null ? var.global_secondary_indexes : {}
+    iterator = "gsi"
+
+    content {
+      name               = gsi.key
+      hash_key           = gsi.value.partition_key
+      range_key          = gsi.value.sort_key
+      projection_type    = gsi.value.projection_type
+      non_key_attributes = gsi.value.non_key_attributes
+      read_capacity      = try(gsi.value.capacity.read.max, null)
+      write_capacity     = try(gsi.value.capacity.write.max, null)
+    }
+  }
+
+  # LOCAL SECONDARY INDEXES
+  dynamic "local_secondary_index" {
+    for_each = var.local_secondary_indexes != null ? var.local_secondary_indexes : {}
+    iterator = "lsi"
+
+    content {
+      name               = lsi.key
+      range_key          = lsi.value.sort_key
+      projection_type    = lsi.value.projection_type
+      non_key_attributes = lsi.value.non_key_attributes
+    }
+  }
+
+  # SSE (required)
+  server_side_encryption {
+    enabled     = true
+    kms_key_arn = var.server_side_encryption_kms_key_arn
+  }
+
+  # TTL
+  dynamic "ttl" {
+    for_each = var.ttl != null ? [var.ttl] : []
+
+    content {
+      enabled        = coalesce(ttl.value.enabled, true)
+      attribute_name = ttl.value.attribute_name
+    }
+  }
+
+  # POINT IN TIME RECOVERY (enabled by default)
+  dynamic "point_in_time_recovery" {
+    for_each = var.enable_point_in_time_recovery != false ? [var.enable_point_in_time_recovery] : []
+
+    content {
+      enabled = true
+    }
+  }
+
+  # RESTORE-TABLE
+  restore_source_name    = lookup(var.restore_table_from, "source_table_name", null)
+  restore_to_latest_time = lookup(var.restore_table_from, "use_latest_recovery_point", null)
+  restore_date_time      = lookup(var.restore_table_from, "restore_date_time", null)
+
+  # STREAM: DynamoDB Table Stream
+  stream_enabled   = try(var.streams.dynamodb_stream_view_type, null) != null
+  stream_view_type = try(var.streams.dynamodb_stream_view_type, null)
+
+  # REGIONAL REPLICAS
+  dynamic "replica" {
+    for_each = var.replicas != null ? var.replicas : {}
+
+    content {
+      region_name            = replica.key
+      propagate_tags         = replica.value.propagate_tags
+      kms_key_arn            = replica.value.kms_key_arn
+      point_in_time_recovery = replica.value.enable_point_in_time_recovery == false # default: true
+    }
+  }
+
+  tags = var.tags
+}
+
+#---------------------------------------------------------------------
+### DynamoDB Table Item
+
+resource "aws_dynamodb_table_item" "list" {
+  count = var.dynamodb_table_items != null ? length(var.dynamodb_table_items) : 0
+
+  table_name = aws_dynamodb_table.this.name
+  hash_key   = aws_dynamodb_table.this.hash_key
+  range_key  = aws_dynamodb_table.this.range_key
+  item       = var.dynamodb_table_items[count.index]
+}
+
+#---------------------------------------------------------------------
+### DynamoDB Stream: Kinesis Data Stream
+
+resource "aws_dynamodb_kinesis_streaming_destination" "list" {
+  count = try(var.streams.kinesis_stream_arn, null) != null ? 1 : 0
+
+  stream_arn = var.streams.kinesis_stream_arn
+  table_name = aws_dynamodb_table.this.name
+}
+
+######################################################################
