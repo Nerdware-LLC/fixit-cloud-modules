6d785efa379aa9a59a730e18f12679bdf64bbc83
trevor-anderson <T.AndersonProperty@gmail.com>
2022-08-05 12:10:40 -0400

    docs(AccBase): update example inputs

--- AWS_AccountBaseline/examples/terraform.tf
+++ AWS_AccountBaseline/examples/terraform.tf
@@ -1,17 +1,18 @@
 ######################################################################
 ### EXAMPLE USAGE: AWS_AccountBaseline
 
-# module "dep" {
-#   /* dependency module values */
-# }
+module "AWS_AccountBaseline" {
+  source = "git@github.com:Nerdware-LLC/fixit-cloud-modules.git//AWS_AccountBaseline"
 
-# locals {
+  # It's not recommended to override the default s3_account_public_access_block values.
+  # Totally fine to not provide any inputs. If you like, tag the default VPC resources:
 
-# }
-
-# module "AWS_VPC" {
-#   source = "git@github.com:Nerdware-LLC/fixit-cloud-modules.git//AWS_AccountBaseline"
-
-# }
+  default_vpc_component_tags = {
+    default_vpc            = { Name = "Default_VPC", isLockedDown = "true" }
+    default_route_table    = { Name = "Default_RouteTable", isLockedDown = "true" }
+    default_network_acl    = { Name = "Default_NACL", isLockedDown = "true" }
+    default_security_group = { Name = "Default_SecGrp", isLockedDown = "true" }
+  }
+}
 
 ######################################################################
--- AWS_AccountBaseline/examples/terragrunt.hcl
+++ AWS_AccountBaseline/examples/terragrunt.hcl
@@ -7,12 +7,18 @@ across multiple resource configs and utilized with a combination of
 include-blocks and/or the read_terragrunt_config() Terragrunt fn. */
 
 #---------------------------------------------------------------------
-### Dependencies
-
-
-#---------------------------------------------------------------------
 ### Inputs
 
+inputs = {
+  # It's not recommended to override the default s3_account_public_access_block values.
+  # Totally fine to not provide any inputs. If you like, tag the default VPC resources:
 
+  default_vpc_component_tags = {
+    default_vpc            = { Name = "Default_VPC", isLockedDown = "true" }
+    default_route_table    = { Name = "Default_RouteTable", isLockedDown = "true" }
+    default_network_acl    = { Name = "Default_NACL", isLockedDown = "true" }
+    default_security_group = { Name = "Default_SecGrp", isLockedDown = "true" }
+  }
+}
 
 ######################################################################
